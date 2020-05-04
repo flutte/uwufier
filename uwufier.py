b71519a0179b2a61ae6f88523caef1e6d7665bc4
BuildTools <unconfigured@null.spigotmc.org>
2020-05-04 18:48:07 +0200

    OwO whats whis

--- uwufier.py
+++ uwufier.py
@@ -1,4 +1,9 @@
+import sys
+
 def uwufy(string):
 	return f"OwO {string.replace('r','w')} >w<"
 
-print(uwufy("Test string cute random words"))
+if not len(sys.argv) == 1:
+	print(uwufy(sys.argv[1]))
+else:
+	print("UwU ewwow: you didn't entew a stwing")
